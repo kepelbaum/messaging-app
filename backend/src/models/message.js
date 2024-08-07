25cbac1a129279dde585d41cbcfa9bcc0712af4a
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-08-07 18:13:07 -0400

    feat: populated mongo schemas with sample data

--- backend/src/models/message.js
+++ backend/src/models/message.js
@@ -5,7 +5,11 @@ const messageSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  user: [{ type: String, required: true }],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Messenger",
+    required: true,
+  },
   // chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat", required: true },
   date: { type: Date, required: true },
 });
